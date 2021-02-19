b7164ac676f968165d6037004415d3fbac472db0
Nikita Babko <niki-babko@yandex.ru>
2021-02-19 20:23:19 +0700

    Linting.

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,19 +1,21 @@
-import React from "react";
-import "./Buttons.scss";
+import React from 'react';
+import './Buttons.scss';
 /**
  * @function Buttons
+ * @param {string} title
+ * @public
  **/
 
 const style = {
-    width: "100px",
+  width: '100px',
 };
 
 const Buttons = ({ className, title }) => {
-    return (
-        <button className={className} size={style}>
-            {title}
-        </button>
-    );
+  return (
+    <button className={className} size={style}>
+      {title}
+    </button>
+  );
 };
 
 export default Buttons;
